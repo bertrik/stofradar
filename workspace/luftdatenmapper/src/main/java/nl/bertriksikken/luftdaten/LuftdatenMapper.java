d0e684a7bfd8201db23109d7a8fc71cefb6d7d00
Bertrik Sikken <bertrik@sikken.nl>
2018-06-12 23:11:00 +0200

    Update for website use.

--- workspace/luftdatenmapper/src/main/java/nl/bertriksikken/luftdaten/LuftdatenMapper.java
+++ workspace/luftdatenmapper/src/main/java/nl/bertriksikken/luftdaten/LuftdatenMapper.java
@@ -2,11 +2,19 @@ package nl.bertriksikken.luftdaten;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -56,10 +64,19 @@ public final class LuftdatenMapper {
 
     public static void main(String[] args) throws IOException {
         LuftdatenMapper luftdatenMapper = new LuftdatenMapper();
-        luftdatenMapper.run("http://api.luftdaten.info");
+        LuftdatenMapperConfig config = new LuftdatenMapperConfig();
+        File configFile = new File("luftdatenmapper.properties");
+        if (configFile.exists()) {
+            LOG.info("Loading config from {}", configFile);
+            config.load(new FileInputStream(configFile));
+        } else {
+            LOG.info("Saving config to {}", configFile);
+            config.save(new FileOutputStream(configFile));
+        }
+        luftdatenMapper.run(config);
     }
 
-    private void run(String url) throws IOException {
+    private void run(ILuftdatenMapperConfig config) throws IOException {
         File inputDir = new File("download");
         File doneDir = new File("processed");
         File outputDir = new File("image");
@@ -68,64 +85,72 @@ public final class LuftdatenMapper {
             throw new IOException("Failed to create directories");
         }
 
-        // schedule the download process
-        executor.scheduleAtFixedRate(() -> downloadFiles(inputDir, url), 0L, 10, TimeUnit.SECONDS);
-
-        // schedule the json -> png process
-        executor.scheduleAtFixedRate(() -> processFiles(inputDir, doneDir, outputDir), 0L, 10L, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(() -> downloadAndProcess(config), 0L, 300L, TimeUnit.SECONDS);
     }
 
-    private void downloadFiles(File downloadDir, String url) {
+    private void downloadAndProcess(ILuftdatenMapperConfig config) {
+        try {
+            File tempDir = new File(config.getIntermediateDir());
+            tempDir.mkdirs();
+
+            // download
+            File jsonFile = downloadFile(tempDir, config.getLuftdatenUrl());
+
+            // create overlay
+            File overlayFile = new File(tempDir, jsonFile.getName() + ".png");
+            ColorMapper colorMapper = new ColorMapper(RANGE);
+            renderDust(jsonFile, overlayFile, colorMapper);
+
+            // create composite
+            File baseMap = new File(config.getBaseMapPath());
+            File compositeFile = new File(config.getOutputPath());
+            compositeFile.getParentFile().mkdirs();
+            composite(config.getCompositeCmd(), overlayFile, baseMap, compositeFile);
+        } catch (IOException e) {
+            LOG.trace("Caught IOException", e);
+            LOG.warn("Caught IOException", e.getMessage());
+        }
+    }
+
+    /**
+     * Downloads a new JSON dust file if necessary.
+     * 
+     * @param downloadDir the directory to download to
+     * @param url the URL to download from
+     * @return the file
+     * @throws IOException
+     */
+    private File downloadFile(File downloadDir, String url) throws IOException {
         ZonedDateTime dt = ZonedDateTime.now(ZoneId.of("UTC"));
         int minute = 5 * (dt.get(ChronoField.MINUTE_OF_HOUR) / 5);
         String fileName = String.format(Locale.US, "%04d%02d%02d_%02d%02d.json", dt.get(ChronoField.YEAR),
                 dt.get(ChronoField.MONTH_OF_YEAR), dt.get(ChronoField.DAY_OF_MONTH), dt.get(ChronoField.HOUR_OF_DAY),
                 minute);
-        try {
-            File file = new File(downloadDir, fileName);
-            if (!file.exists()) {
-                ILuftdatenRestApi restApi = LuftDatenDataApi.newRestClient(url, 3000);
-                LuftDatenDataApi api = new LuftDatenDataApi(restApi);
-                LOG.info("Downloading new dataset");
-                api.downloadDust(file);
-            }
-        } catch (IOException e) {
-            LOG.warn("Caught {}", e.getMessage());
+        File file = new File(downloadDir, fileName);
+        if (!file.exists()) {
+            ILuftdatenRestApi restApi = LuftDatenDataApi.newRestClient(url, 3000);
+            LuftDatenDataApi api = new LuftDatenDataApi(restApi);
+            LOG.info("Downloading new dataset to {}", file);
+            api.downloadDust(file);
         }
+        return file;
     }
 
-    private void processFiles(File inputDir, File doneDir, File outputDir) {
-        File[] files = inputDir.listFiles((f) -> filterByExtension(f, ".json"));
-        for (File file : files) {
-            try {
-                File outFile = new File(outputDir, file.getName() + ".png");
-                if (!outFile.exists()) {
-                    LOG.info("Processing file {}", file);
-                    processFile(file, outFile);
-                }
-            } catch (IOException e) {
-                LOG.warn("Caught {}", e.getMessage());
-            } catch (Exception e) {
-                LOG.warn("Caught {}", e.getMessage());
-            } finally {
-                // move to done dir
-                // LOG.info("Moving file {} to {}", file, doneDir);
-                // file.renameTo(new File(doneDir, file.getName()));
-            }
-        }
-    }
-
-    private boolean filterByExtension(File file, String extension) {
-        return file.getName().endsWith(extension);
-    }
-
-    private void processFile(File file, File outFile) throws IOException {
+    /**
+     * Renders a JSON file to a PNG.
+     * 
+     * @param jsonFile the JSON file
+     * @param pngFile the PNG file
+     * @param colorMapper the color mapper
+     * @throws IOException
+     */
+    private void renderDust(File jsonFile, File pngFile, ColorMapper colorMapper) throws IOException {
         int width = 60;
         int height = 80;
 
         // read file
         ObjectMapper mapper = new ObjectMapper();
-        DataPoints dataPoints = mapper.readValue(file, DataPoints.class);
+        DataPoints dataPoints = mapper.readValue(jsonFile, DataPoints.class);
         DataPoints filtered = filterBySensorType(dataPoints, 14);
 
         // interpolate over grid
@@ -135,12 +160,46 @@ public final class LuftdatenMapper {
         // convert to color PNG
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         WritableRaster raster = image.getRaster();
-        ColorMapper colorMapper = new ColorMapper(RANGE);
         colorMapper.map(field, raster);
 
         // save it
-        LOG.info("Writing to {}", outFile);
-        ImageIO.write(image, "png", outFile);
+        LOG.info("Writing to {}", pngFile);
+        ImageIO.write(image, "png", pngFile);
+    }
+
+    /**
+     * Composites a combined image of a fine dust overlay over a base map.
+     * 
+     * @param command path to the imagemagick 'composite command'
+     * @param overlay the dust overlay image
+     * @param baseMap the base map image
+     * @param outFile the combined image
+     */
+    private void composite(String command, File overlay, File baseMap, File outFile) {
+        List<String> arguments = new ArrayList<>();
+        arguments.add(command);
+        arguments.addAll(Arrays.asList("-compose", "over"));
+        arguments.addAll(Arrays.asList("-geometry", "800x600"));
+        arguments.add(overlay.getAbsolutePath());
+        arguments.add(baseMap.getAbsolutePath());
+        arguments.add(outFile.getAbsolutePath());
+        ProcessBuilder pb = new ProcessBuilder(arguments);
+        try {
+            Process process = pb.start();
+            int exitValue = process.waitFor();
+            if (exitValue != 0) {
+                InputStream is = process.getErrorStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    LOG.info("line: {}", line);
+                }
+            }
+            LOG.info("Process ended with {}", exitValue);
+        } catch (Exception e) {
+            LOG.trace("Caught IOException", e);
+            LOG.warn("Caught IOException: {}", e.getMessage());
+        }
     }
 
 }
