8390912e1d62f516e57a10fd927a5284cb2a09e4
Bertrik Sikken <bertrik@sikken.nl>
2021-03-04 23:33:12 +0100

    Add animation script.

---
+++ luftdatenmapper/src/dist/animate.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+
+from pathlib import Path
+
+def build_cmd_list(dirpath):
+    paths = sorted(Path(dirpath).iterdir(), key=os.path.getmtime)
+    cmdlist = [];
+    for p in paths:
+        if p.name.endswith(".png"):
+            cmdlist.append(f"file {p.name}")
+            cmdlist.append("duration 0.1")
+    return cmdlist
+
+
+def main():
+    """ main entry point """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dir", help="The directory with input files", default=".")
+    parser.add_argument("-o", "--output", help="The output file", default="out.mp4")
+    args = parser.parse_args()
+
+    # build the command file
+    cmd_list = build_cmd_list(args.dir)
+    cmdfile_name = "cmdfile.txt"
+    with open(os.path.join(args.dir, cmdfile_name), "w") as cmdfile:
+        cmdfile.write('\n'.join(cmd_list))
+    
+    # invoke ffmpeg
+    params = ["ffmpeg", "-y", "-f", "concat", "-i", cmdfile_name, args.output]
+    subprocess.run(params, cwd = args.dir)
+    
+if __name__ == "__main__":
+    main()
+
