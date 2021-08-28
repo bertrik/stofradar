b3916f61dfbe1c919b715f934bd58dc8600603a2
Bertrik Sikken <bertrik@sikken.nl>
2021-08-28 09:03:26 +0200

    Make cyan (PM10=25ugm3) a little less bright in color scaling

--- luftdatenmapper/src/main/java/nl/bertriksikken/luftdaten/LuftdatenMapper.java
+++ luftdatenmapper/src/main/java/nl/bertriksikken/luftdaten/LuftdatenMapper.java
@@ -77,7 +77,7 @@ public final class LuftdatenMapper {
 
     private static final ColorPoint[] RANGE = new ColorPoint[] {
             new ColorPoint(0, new int[] { 0x00, 0x00, 0xFF, 0x00 }), // transparent blue
-            new ColorPoint(25, new int[] { 0x00, 0xFF, 0xFF, 0xC0 }), // cyan
+            new ColorPoint(25, new int[] { 0x00, 0x80, 0x80, 0xC0 }), // cyan
             new ColorPoint(50, new int[] { 0xFF, 0xFF, 0x00, 0xC0 }), // yellow
             new ColorPoint(100, new int[] { 0xFF, 0x00, 0x00, 0xC0 }), // red
             new ColorPoint(200, new int[] { 0xFF, 0x00, 0xFF, 0xC0 }), // purple
