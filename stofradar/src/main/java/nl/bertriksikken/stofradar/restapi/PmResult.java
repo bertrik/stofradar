78860b2127518dc981bc15bd56c7e8e7864d854e
Bertrik Sikken <bertrik@sikken.nl>
2022-04-25 20:43:50 +0200

    Allow empty result for NaN, limit PM2.5 decimals to two.

--- stofradar/src/main/java/nl/bertriksikken/stofradar/restapi/PmResult.java
+++ stofradar/src/main/java/nl/bertriksikken/stofradar/restapi/PmResult.java
@@ -1,19 +1,26 @@
 package nl.bertriksikken.stofradar.restapi;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(Include.NON_NULL)
 public final class PmResult {
     
     @JsonProperty("pm2.5")
-    private double pm2_5;
+    private BigDecimal pm2_5;
     
-    // jackson constructor
     PmResult() {
-        this(0.0);
+        // jackson constructor
     }
     
     PmResult(double pm2_5) {
-        this.pm2_5 = pm2_5;
+        if (Double.isFinite(pm2_5)) {
+            this.pm2_5 = BigDecimal.valueOf(pm2_5).setScale(2, RoundingMode.HALF_UP);
+        }
     }
     
 }
---
+++ stofradar/src/test/java/nl/bertriksikken/stofradar/restapi/PmResultTest.java
@@ -0,0 +1,20 @@
+package nl.bertriksikken.stofradar.restapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class PmResultTest {
+
+    @Test
+    public void testSerialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        // ordinary number
+        Assert.assertEquals("{\"pm2.5\":12.46}", mapper.writeValueAsString(new PmResult(12.45678)));
+        // NaN
+        Assert.assertEquals("{}", mapper.writeValueAsString(new PmResult(Double.NaN)));
+    }
+
+}
