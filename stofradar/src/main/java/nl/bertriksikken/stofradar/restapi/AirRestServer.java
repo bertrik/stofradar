52a48800cdf3ac42be542260b100429feef669d6
Bertrik Sikken <bertrik@sikken.nl>
2022-10-23 17:13:04 +0200

    Update to Jersey 3 / Jetty 11.

--- stofradar/src/main/java/nl/bertriksikken/stofradar/restapi/AirRestServer.java
+++ stofradar/src/main/java/nl/bertriksikken/stofradar/restapi/AirRestServer.java
@@ -1,6 +1,7 @@
 package nl.bertriksikken.stofradar.restapi;
 
 import java.io.IOException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
@@ -8,19 +9,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ServerProperties;
-import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import es.moki.ratelimitj.core.limiter.request.RequestLimitRule;
 import es.moki.ratelimitj.core.limiter.request.RequestRateLimiter;
 import es.moki.ratelimitj.inmemory.request.InMemorySlidingWindowRequestRateLimiter;
+import jakarta.ws.rs.core.UriBuilder;
 import nl.bertriksikken.stofradar.render.SensorValue;
 
 public final class AirRestServer {
@@ -61,26 +59,11 @@ public final class AirRestServer {
 
     private Server createRestServer(int port, String contextPath, Class<?> clazz) {
         LOG.info("Setting up Air REST service on {}", port);
-        Server server = new Server(port);
 
-        // setup context
-        ServletContextHandler context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+        URI uri = UriBuilder.fromUri("http://localhost").port(port).build();
+        ResourceConfig config = new ResourceConfig(AirRestApi.class);
+        Server server = JettyHttpContainerFactory.createServer(uri, config);
 
-        // suppress sending the exact jetty version
-        for (Connector connector : server.getConnectors()) {
-            for (org.eclipse.jetty.server.ConnectionFactory connectionFactory : connector.getConnectionFactories()) {
-                if (connectionFactory instanceof HttpConnectionFactory) {
-                    HttpConnectionFactory httpConnectionFactory = (HttpConnectionFactory) connectionFactory;
-                    httpConnectionFactory.getHttpConfiguration().setSendServerVersion(false);
-                }
-            }
-        }
-
-        // setup web services container
-        ServletHolder sh = new ServletHolder(ServletContainer.class);
-        sh.setInitParameter(ServerProperties.PROVIDER_CLASSNAMES, clazz.getCanonicalName());
-        context.addServlet(sh, contextPath + "/*");
-        server.setHandler(context);
         return server;
     }
 
