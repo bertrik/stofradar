cd1013767239a0032dcc3359a1072d10e43a6f04
Bertrik Sikken <bertrik@sikken.nl>
2023-10-15 17:21:23 +0200

    Add somewhat meaningful user-agent in the request to sensor.community.

--- stofradar/src/main/java/nl/bertriksikken/stofradar/senscom/SensComDataApi.java
+++ stofradar/src/main/java/nl/bertriksikken/stofradar/senscom/SensComDataApi.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import nl.bertriksikken.stofradar.config.HostConnectionConfig;
 import nl.bertriksikken.stofradar.senscom.dto.DataPoint;
+import okhttp3.Interceptor.Chain;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -19,35 +22,42 @@ public final class SensComDataApi {
 
     private static final Logger LOG = LoggerFactory.getLogger(SensComDataApi.class);
 
-    private final ISensComRestApi api;
+    private static final String USER_AGENT = "github.com/bertrik/stofradar";
+
+    private final ISensComRestApi restApi;
 
     /**
      * Constructor.
      * 
-     * @param api the REST API
+     * @param restApi the REST API
      */
-    SensComDataApi(ISensComRestApi api) {
-        this.api = api;
+    SensComDataApi(ISensComRestApi restApi) {
+        this.restApi = Objects.requireNonNull(restApi);
     }
 
     public static SensComDataApi create(HostConnectionConfig config) {
         LOG.info("Creating new REST client for URL '{}' with timeout {}", config.getUrl(), config.getTimeout());
         Duration timeout = config.getTimeout();
-        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(timeout).readTimeout(timeout)
-                .writeTimeout(timeout).build();
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(SensComDataApi::addUserAgent)
+                .connectTimeout(timeout).readTimeout(timeout).writeTimeout(timeout).build();
         Retrofit retrofit = new Retrofit.Builder().baseUrl(config.getUrl())
                 .addConverterFactory(JacksonConverterFactory.create()).client(client).build();
         ISensComRestApi restApi = retrofit.create(ISensComRestApi.class);
         return new SensComDataApi(restApi);
     }
 
+    private static okhttp3.Response addUserAgent(Chain chain) throws IOException {
+        Request userAgentRequest = chain.request().newBuilder().header("User-Agent", USER_AGENT).build();
+        return chain.proceed(userAgentRequest);
+    }
+
     public List<DataPoint> downloadDust() throws IOException {
         List<DataPoint> entries = new ArrayList<>();
-        Response<List<DataPoint>> response = api.getAverageDustData().execute();
+        Response<List<DataPoint>> response = restApi.getAverageDustData().execute();
         if (response.isSuccessful()) {
             entries.addAll(response.body());
         } else {
-            LOG.warn("Download failed: {}", response.errorBody());
+            LOG.warn("Download failed: {} - {}", response.message(), response.errorBody());
         }
         return entries;
     }
